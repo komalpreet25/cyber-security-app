3e4eacc2007024635371d4593445dcb5740436aa
komalpreet25 <komalsahota138@gmail.com>
2026-04-09 15:14:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ Built using Python and Streamlit.
 
 # 🔐 PASSWORD PAGE
 elif st.session_state.page == "password":
-    set_bg("#11998e", "#734b6d")   # Purple
+    set_bg("#dbeafe", "#bfdbfe")   # Purple
     st.title("🔐 Password Checker")
     st.markdown("### 📌 Check how strong your password is")
     st.markdown("""
